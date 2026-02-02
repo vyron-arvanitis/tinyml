7231718435dc277d25ea9d263c64058ea509a25a
vyron-arvanitis <vyronas.arvanitis@gmail.com>
2026-02-03 00:10:54 +0100

    workign examples of random_gen functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstddef>
 #include "matrix.h"
-
+#include "random_gen.h"
 
 int main()
 {
@@ -10,7 +10,19 @@ int main()
     std::cout << "The first element of the matrix is : " << m.get(0, 0) << "\n";
     std::cout << "Setting the firstelemtn to : 100 \n";
     m.set(0, 0, 100);
-    std::cout << "The first element of the matrix is : " << m.get(0, 0);
+    std::cout << "The first element of the matrix is : " << m.get(0, 0) <<"\n";
+    std::cout << "Testing THE RANDOM GEN FILE \n";
+    
+    tinyml::RNG rng(123); // or std::random_device{}()
+    tinyml::Matrix n = tinyml::random_normal(3, 3, 0.0, 1.0, rng);
+    for (std::size_t i = 0; i < n.rows(); ++i) {
+        for (std::size_t j = 0; j < n.cols(); ++j) {
+            std::cout << n.get(i, j) << (j + 1 == n.cols() ? '\n' : ' ');
+        }
+    }
+
+    std::cout << "The first element of the matrix is : " << n.get(0, 0) <<"\n";
+
 
     return 0;
 }
